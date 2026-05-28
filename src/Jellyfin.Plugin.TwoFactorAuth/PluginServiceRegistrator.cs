73a3c4086cc154a502276f0c9a82f4b4e7aced54
ZL154 <74380507+ZL154@users.noreply.github.com>
2026-05-28 14:28:29 +0100

    chore(v2.5.0): register StepUpService

--- src/Jellyfin.Plugin.TwoFactorAuth/PluginServiceRegistrator.cs
+++ src/Jellyfin.Plugin.TwoFactorAuth/PluginServiceRegistrator.cs
@@ -16,6 +16,7 @@ public class PluginServiceRegistrator : IPluginServiceRegistrator
     {
         serviceCollection.AddSingleton<UserTwoFactorStore>();
         serviceCollection.AddSingleton<ChallengeStore>();
+        serviceCollection.AddSingleton<StepUpService>();
         serviceCollection.AddSingleton<TotpService>();
         serviceCollection.AddSingleton<EmailOtpService>();
         serviceCollection.AddSingleton<DeviceTokenService>();
